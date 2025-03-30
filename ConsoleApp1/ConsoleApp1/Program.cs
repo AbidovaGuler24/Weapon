0b560573807ab4146e15b65b79b00673eef63673
AbidovaGuler <gullarabidova@gmail.com>
2025-03-30 22:19:33 +0400

    weopen

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp1
+{
+     class Program
+    {
+        static void Main()
+        {
+            Weapon gun = new Weapon(30, "single");
+            bool running = true;
+
+            while (running)
+            {
+                Console.Write("Enter command (1-atış, 2-qalan, 3-yenidən yükləmə, 4-dəyişmə rejimi,5-end): ");
+                string command = Console.ReadLine();
+                switch (command)
+                {
+                    case "1":
+                        gun.Shood();
+                        break;
+
+                    case "2":
+                        Console.WriteLine(gun.GetRemainBulletCount());
+                        break;
+                    case "3":
+                        gun.Reload();
+                        break;
+
+                    case "4":
+                        gun.ChangeFireMode();
+                        break;
+                    case "5":
+                        Console.WriteLine("end");
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Yanlis");
+                        break;
+                }
+            }
+
+
+
+         }
+
+
+      }
+ }
---
+++ ConsoleApp1/ConsoleApp1/Weapon.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Weapon
+    {
+       
+            private int capasity;
+            private int bullets;
+            private string fireMode;
+            public Weapon(int capacity, string fireMode)
+            {
+                this.capasity = capacity;
+                this.fireMode = fireMode;
+                this.bullets = capacity;
+                while (capacity <= 0)
+                {
+                    Console.WriteLine("dəyər daxil edin");
+                    capacity = Convert.ToInt32(Console.ReadLine());
+                }
+
+
+                while (fireMode != "single" && fireMode != "auto")
+                {
+                    Console.WriteLine("tək və ya avtomatik  daxil edin:");
+                    fireMode = Console.ReadLine();
+                }
+            }
+            public void Shood()
+            {
+                if (bullets==0)
+                {
+                    Console.WriteLine("güllə yoxdu");
+
+                }
+                else
+                {
+                    if (fireMode=="single")
+                    {
+                        bullets--;
+                        Console.WriteLine("güllə atıldı");
+                    }
+                    else
+                    {
+                        Console.WriteLine("gulle yoxdu");
+                        bullets = 0;
+                    }
+
+                }
+
+
+            }
+            public int GetRemainBulletCount()
+            {
+                return capasity-bullets;
+
+            }
+            public void Reload()
+            {
+                bullets = capasity;
+                Console.WriteLine("Doldu");
+            }
+            public void ChangeFireMode()
+            {
+                if (fireMode == "single")
+                {
+                    fireMode = "auto";
+                }
+                else
+                {
+                    fireMode = "single";
+                }
+            }
+
+     }
+
+    
+}
